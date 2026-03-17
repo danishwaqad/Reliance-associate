74fb1406143296612f26f40f1e457fc832562d73
danish waqad <dev14@nellyismarketing.com>
2026-03-17 15:58:38 +0500

    app changings

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Header from './components/Header'
 import About from './components/About'
 import Projects from './components/Projects'
-import Testimonials from './components/testimonials'
+import Testimonials from './components/Testimonials'
 import Contact from './components/Contact'
 import { ToastContainer } from 'react-toastify';
 import Footer from './components/Footer'
